957dc59c8cbed3c0ae3a4f25f7d112bb946b3e79
devteam <devteam@digiolive.ai>
2024-09-05 16:21:40 +0500

    Fix: Loader fixed

--- components/loader.js
+++ components/loader.js
@@ -2,14 +2,14 @@ import Modal from "react-responsive-modal";
 import { Spin } from "antd";
 
 export default function Loader(props) {
-    return (
-        <Modal
-            open={props.loading}
-            classNames={"loading-modal"}
-            center
-            closeOnOverlayClick={false}
-        >
-            <Spin />
-        </Modal>
-    )
+  return (
+    <Modal
+      open={props.loading}
+      classNames={"loading-modal"}
+      center
+      closeOnOverlayClick={false}
+    >
+      <Spin />
+    </Modal>
+  );
 }
